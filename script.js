afe2d1a27defd191c8549c5a1492d7ae4c2dbc70
uddhavk <164346647+uddhavk@users.noreply.github.com>
2024-07-29 21:45:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,101 @@ designerDesign.addEventListener('mouseover',function(){
 designerDesign.addEventListener('mouseleave',function(){
     design.style.display = 'none';
 });
+
+
+
+
+
+
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 8,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 0, // No delay between transitions
+        disableOnInteraction: true // Swiper will continue autoplay after user interactions
+    },
+    speed: 35000, // Duration of transition in milliseconds
+    mousewheel: true,
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        375: {
+            slidesPerView: 1,
+        },
+        425: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 8,
+        }
+    }
+});
+
+
+
+
+
+
+
+// let swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 8,
+//     spaceBetween: 20,
+//     loop: true,
+//     autoplay: true,
+//     mousewheel: true,
+
+//     breakpoints: {
+//         320: {
+//             slidesPerView: 3,
+//         },
+//         375: {
+//             slidesPerView: 4,
+//         },
+//         425: {
+//             slidesPerView: 4,
+//         },
+//         768: {
+//             slidesPerView:4,
+//         },
+//         1024: {
+//             slidesPerView:8,
+//         }
+        
+//     }
+// })
+
+let uddhav = new Swiper(".mySwiper2", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    // loop: true,
+    // autoplay: true,
+    mousewheel: true,
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        375: {
+            slidesPerView: 1,
+        },
+        425: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView:2,
+        },
+        1024: {
+            slidesPerView:4,
+        }
+        
+    }
+})
+
+
